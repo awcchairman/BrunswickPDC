cd181fba08d2c84aae85a686655a6741a67d4d2b
awcchairman <45357963+awcchairman@users.noreply.github.com>
2019-01-01 05:39:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,101 @@
-
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+$('[data-fancybox="images"]').fancybox({
+  buttons : [
+    'slideShow',
+    'share',
+    'zoom',
+    'fullScreen',
+    'close'
+  ],
+  thumbs : {
+    autoStart : true
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+});
+// Init fancybox
+$().fancybox({
+  selector : '.imglist a:visible',
+  thumbs   : {
+    autoStart : true
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+});
+
+// Simple filter
+$('#filter').on('change', function() {
+  var $visible, val = this.value;
+
+  if (val) {
+    $visible = $('.imglist a').hide().filter('.' + val);
+
+  } else {
+    $visible = $('.imglist a');
+  }
+
+  $visible.show();
+});
+var gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  uglify = require("gulp-uglify"),
+  rename = require("gulp-rename"),
+  cssnano = require("gulp-cssnano"),
+  autoprefixer = require("gulp-autoprefixer"),
+  header = require("gulp-header"),
+  replace = require("gulp-replace");
+
+var pkg = require("./package.json");
+var banner = [
+  "// ==================================================",
+  "// fancyBox v${pkg.version}",
+  "//",
+  "// Licensed GPLv3 for open source use",
+  "// or fancyBox Commercial License for commercial use",
+  "//",
+  "// http://fancyapps.com/fancybox/",
+  "// Copyright ${new Date().getFullYear()} fancyApps",
+  "//",
+  "// ==================================================",
+  ""
+].join("\n");
+
+// Concatenate & Minify JS
+
+gulp.task("scripts", function() {
+  return gulp
+    .src([
+      "src/js/core.js",
+      "src/js/media.js",
+      "src/js/guestures.js",
+      "src/js/slideshow.js",
+      "src/js/fullscreen.js",
+      "src/js/thumbs.js",
+      "src/js/hash.js",
+      "src/js/wheel.js"
+    ])
+    .pipe(concat("jquery.fancybox.js"))
+    .pipe(replace(/({fancybox-version})/g, pkg.version))
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest("dist"))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(uglify())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(gulp.dest("dist"));
+});
+
+// Compile CSS
+
+gulp.task("css", function() {
+  return gulp
+    .src("src/css/*.css") // Gets all files src/css
+    .pipe(
+      autoprefixer({
+        browsers: ["last 5 versions"],
+        cascade: false
+      })
+    )
+    .pipe(concat("jquery.fancybox.css"))
+    .pipe(gulp.dest("dist"))
+    .pipe(rename({suffix: ".min"}))
+    .pipe(cssnano({zindex: false}))
+    .pipe(gulp.dest("dist"));
+});
+
+// Default Task
+gulp.task("default", ["scripts", "css"]);
